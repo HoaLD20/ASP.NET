29ea657e5d3bc19108e8833e23d71f4b0b9dad16
root <leduchoa911@gmail.com>
2020-09-24 14:27:29 +0700

    ok

--- SeleniumHowKTeam/MainWindow.xaml.cs
+++ SeleniumHowKTeam/MainWindow.xaml.cs
@@ -29,17 +29,24 @@ namespace SeleniumHowKTeam
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             FirefoxDriver firefoxDriver = new FirefoxDriver();
+            //bài 1
+
             //firefoxDriver.Navigate().GoToUrl("ww.fb.com");
             //firefoxDriver.Url = "https://www.facebook.com/";
-            firefoxDriver.Url = "https://www.howkteam.vn/";
-            firefoxDriver.Navigate();
+            //firefoxDriver.Url = "https://www.howkteam.vn/";
+            //firefoxDriver.Navigate();
 
             //tìm đối tượng theo ID
-            var searchBar = firefoxDriver.FindElementById("fa fa-search");
+            //tìm theo class name chỉ tìm dc với những tên ko có khoảng trắng
+            //var searchBar = firefoxDriver.FindElementById("q");
             //gõ giá trị cần tìm vào thanh tìm kiếm
-            searchBar.Click();
+            //searchBar.SendKey("test");
+            //searchBar.Click();
 
-           
+            //bài 2
+
+            firefoxDriver.ExecuteScript("var content = document.getElementsByClassName('contentpagetop')[0].children[0].innerHTML;alert(content);");
+
         }
     }
 }
