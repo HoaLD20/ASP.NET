a38bef21286e827be9f55828bce21797e0d2a00d
root <leduchoa911@gmail.com>
2020-10-04 21:04:47 +0700

    practice lab

---
+++ PracticeLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
