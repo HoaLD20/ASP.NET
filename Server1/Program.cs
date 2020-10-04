116206279d149bc477429d3018a85b4f163539bc
root <leduchoa911@gmail.com>
2020-10-04 20:46:44 +0700

    ok

---
+++ Server1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server1
+{
+    class Program
+    {
+        private IPAddress myip;
+        private int port;
+        private bool serverStatus;
+        private TcpListener tcpListener;
+        private Socket socketforserver;
+        private NetworkStream networkStream;
+        private StreamWriter streamWriter;
+
+        public IPAddress Myip { get => myip; set => myip = value; }
+        public int Port { get => port; set => port = value; }
+        public bool ServerStatus { get => serverStatus; set => serverStatus = value; }
+        public TcpListener TcpListener { get => tcpListener; set => tcpListener = value; }
+        public Socket Socketforserver { get => socketforserver; set => socketforserver = value; }
+        public NetworkStream NetworkStream { get => networkStream; set => networkStream = value; }
+        public StreamWriter StreamWriter { get => streamWriter; set => streamWriter = value; }
+
+        public Program()
+        {
+        }
+        /*
+        public Program(IPAddress myip, int port, bool serverStatus, TcpListener tcpListener, Socket socketClient, NetworkStream networkStream, StreamWriter streamWriter)
+        {
+            this.Myip = myip;
+            this.Port = port;
+            this.ServerStatus = serverStatus;
+            this.TcpListener = tcpListener;
+            this.SocketClient = socketClient;
+            this.NetworkStream = networkStream;
+            this.StreamWriter = streamWriter;
+        }
+        */
+
+        public Program(IPAddress myip, int port)
+        {
+            ServerStatus = true;
+            this.myip = myip;
+            this.port = port;
+        }
+
+        public void startListener()
+        {
+            try
+            {
+                socketforserver = new TcpListener(Myip, Port);
+                tcpListener.Start();
+            }
+            catch
+            {
+
+            }
+        }
+
+
+    }
+}
---
+++ Thread/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread2
+{
+
+    
+    class Program
+    {
+        private static void Sometaskneed()
+        {
+            Console.WriteLine("waitting for thread");
+            Thread.Sleep(5000);
+                }
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(Sometaskneed));
+            thread.Start();
+            thread.Join();//
+            Console.WriteLine("task ok");
+            Console.ReadLine();
+        }
+    }
+}
